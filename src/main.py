e088b94671bb3ca683e16cb06c8fa8d073f2091f
alan-desktop <alann.maman@gamail.com>
2025-02-25 20:56:12 -0300

    default host

--- src/main.py
+++ src/main.py
@@ -225,4 +225,4 @@ async def perform_similarity_search(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
